8909bb686c00d13e7040061967999cd301c94205
OlguD <olgudegirmenci34@gmail.com>
2025-04-21 19:48:53 +0300

    frontend folder created with vite React

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <></>;
+  return (
+    <div className="bg-black text-white text-center">
+      <h1>Welcome to Schedulify</h1>
+    </div>
+  );
 }
 
 export default App;
